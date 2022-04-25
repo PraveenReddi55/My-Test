9d275b261c5aa2828d4794c915a35cf7ad3225c1
PraveenReddi55 <102969058+PraveenReddi55@users.noreply.github.com>
2022-04-25 11:04:27 +0530

    Update mytest.py

--- mytest.py
+++ mytest.py
@@ -12,7 +12,7 @@ option = st.sidebar.selectbox(
 if option=='line chart':
     chart_data = pd.DataFrame(
     np.random.randn(20, 3),
-    columns=['a', 'b', 'c'])
+    columns=['alpha', 'beta', 'delta'])
 
     st.line_chart(chart_data)
 
